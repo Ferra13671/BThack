06e117c2b413adf61b65930371e664231bd37c16
Ferra <ferra13671@gmail.com>
2024-09-15 02:21:34 +0300

    Delete src/main/java/com/ferra13671/BThack directory

--- src/main/java/com/ferra13671/BThack/CommandSystem/Exception/BadArgumentSizeException.java
+++
@@ -1,6 +0,0 @@
-package com.ferra13671.BThack.api.CommandSystem.Exception;
-
-public class BadArgumentSizeException extends CommandIllegalArgumentException {
-    public BadArgumentSizeException() { this(""); }
-    public BadArgumentSizeException(String message) { super(message); }
-}
--- src/main/java/com/ferra13671/BThack/Events/Events/Entity/EntityMotionEvent.java
+++
@@ -1,6 +0,0 @@
-package com.ferra13671.BThack.Events.Events.Entity;
-
-import com.ferra13671.BThack.Events.MegaEvents.Event;
-
-public class EntityMotionEvent extends Event {
-}
